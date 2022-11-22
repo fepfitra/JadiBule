05fdebf97d9d243705ac7301ea88c61826d1a1cb
Yazid Rizki <107790502+Yarizk@users.noreply.github.com>
2022-11-22 09:11:34 +0000

    nambahin read

--- main.cpp
+++ main.cpp
@@ -1,87 +1,128 @@
-#include <iostream>
+
 #include <fstream>
+#include <iostream>
 using namespace std;
 string baris;
 int indeks;
 ofstream OFLASHCARD;
 ifstream IFLASHCARD;
 struct rogerSumatera {
-	int urutan;
-	string vocab;
-	string arti;
+  int urutan;
+  int tingkatHafalan=0;
+  string vocab;
+  string arti;
 };
 rogerSumatera kosakata[100000];
 void mainMenuTop() {
-	cout << "=====\t\tWelcome to JADI BULE !\t\t=====\n\n";
-	cout << "Option\t:\n";
-	cout << "1-ADD Vocabulary\n";
-	cout << "2-DELETE Vocabulary\n";
-	cout << "3-SEARCH Vocabulary\n";
-	cout << "4-REVIEW Vocabulary\n";
-	cout << "5-TEST Vocabulary\n";
+  cout << "=====\t\tWelcome to JADI BULE !\t\t=====\n\n";
+  cout << "Option\t:\n";
+  cout << "1-ADD Vocabulary\n";
+  cout << "2-SEARCH Vocabulary\n";
+  cout << "3-REVIEW Vocabulary\n";
+  cout << "4-TEST Vocabulary\n";
 }
 void mainMenuBottom() {
-	cout << "Choose any option by inputting the number \n";
-	cout << "Your choice : ";
+  cout << "Choose any option by inputting the number \n";
+  cout << "Your choice : ";
 }
 void buffer() {
-	for (int i=0; i<10; i++) {
-		cout << "\n";
-	}
+  for (int i = 0; i < 10; i++) {
+    cout << "\n";
+  }
 }
 void clearScreen() {
-	for (int i=0; i<50; i++) {
-		cout << "\n";
-	}
+  // for (int i = 0; i < 50; i++) {
+  //   cout << "\n";
+  // }
+  #ifdef WINDOWS
+    std::system("cls");
+  #else
+    // Assume POSIX
+    std::system ("clear");
+  #endif
 }
 
 void menuADD() {
-	cout << "Input format : {vocabulary}${meaning}\n";
-	cout << "Example : Real$Nyata\n";
-	buffer();
-	OFLASHCARD.open("kartu1.txt",ios::out | ios::trunc);
-	cout << "Enter the input : ";
-		
+  cout << "Input format : {vocabulary}${meaning}\n";
+  cout << "Example : Real$Nyata\n";
+  buffer();
+
+  cout << "Enter the input : ";
 }
-void DELETE() {
+void read(){
+  IFLASHCARD.open("kartu1.txt");
+  while (getline(IFLASHCARD, baris)) {
+    indeks++;
+    kosakata[indeks].urutan = indeks;
+    kosakata[indeks].vocab = baris.substr(0, baris.find("$"));
+    kosakata[indeks].arti = baris.substr(baris.find("$") + 1, baris.find("#")-baris.find("$")-1);
+    kosakata[indeks].tingkatHafalan = stoi(baris.substr(baris.find("#") + 1, baris.find("%")-baris.find("#")-1));
+    
+  }
+  IFLASHCARD.close();
+
+  // for (int i = 1; i <= indeks; i++) {
+  //   cout << kosakata[i].urutan << ". " << kosakata[i].vocab << " : " << kosakata[i].arti << kosakata[i].tingkatHafalan << endl;
+  // }
 }
-void SEARCH() {
+void search() {
+  
 }
-void REVIEW() {
+void review() {}
+void test() {
+  
 }
-void TEST() {
+void add(){
+  int panjang;
+  menuADD();
+  OFLASHCARD.open("kartu1.txt", ios::app);
+  getline(cin, baris);
+  panjang = baris.length();
+  for (int i = 0; i < panjang; i++) {
+    if (baris[i] == '$') {
+      kosakata[indeks].vocab = baris.substr(0, i);
+      kosakata[indeks].arti = baris.substr(i + 1, panjang - i - 1);
+      kosakata[indeks].urutan = indeks + 1;
+      OFLASHCARD << kosakata[indeks].vocab << "$";
+      OFLASHCARD << kosakata[indeks].arti << "#";
+      OFLASHCARD << kosakata[indeks].tingkatHafalan << "\n";
+      indeks++;
+    }
+  }
+  OFLASHCARD.close();
 }
 
 int main() {
-	
-
-	OFLASHCARD.open("kartu1.txt");
-	mainMenuTop();
-	buffer();
-	mainMenuBottom();
-	int choice;
-	cin >> choice;
-	switch (choice) {
-		case 1 : clearScreen(); menuADD(); goto tambah; break;
-		case 2 : clearScreen(); DELETE(); break;
-		case 3 : clearScreen(); SEARCH(); break;
-		case 4 : clearScreen(); REVIEW(); break;
-		case 5 : clearScreen(); TEST(); break;
-		default : clearScreen(); break;
-	}
-	tambah :
-		getline(cin, baris);
-		int panjang=baris.length();
-		for (int i=0; i<panjang; i++) {
-			if (baris[i]=='$') {
-				kosakata[indeks].vocab=baris.substr(0,i);
-				kosakata[indeks].arti=baris.substr(i+1, panjang-i-1);
-				kosakata[indeks].urutan=indeks+1;
-				indeks++;
-			}
-		}	
+  bool running = true;
+  while(running){
+    clearScreen();
+    mainMenuTop();
+    buffer();
+    mainMenuBottom();
+    int choice;
+    cin >> choice;
+    cin.ignore();
+    switch (choice) {
+    case 1:
+      add();
+      break;
+    case 2:
+      read();
+      // search();
+      break;
+    case 3:
+      review();
+      break;
+    case 4:
+      test();
+      break;
+    case 5:
+      running = false;
+      break;
+    default:
+      cout << "Invalid input";
+    }
+  }
 }
-	
-	
-	
-	
+
+
