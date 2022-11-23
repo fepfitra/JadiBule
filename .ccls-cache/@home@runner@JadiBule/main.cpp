9221243a9692373b0756eab85b502d0273ee893e
Yazid Rizki <107790502+Yarizk@users.noreply.github.com>
2022-11-23 09:10:54 +0000

    nambahin buat test nya

--- .ccls-cache/@home@runner@JadiBule/main.cpp
+++ .ccls-cache/@home@runner@JadiBule/main.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 using namespace std;
 string baris;
-int indeks;
+int indeks = 0;
 ofstream OFLASHCARD;
 ifstream IFLASHCARD;
 struct rogerSumatera {
   int urutan;
-  int tingkatHafalan=0;
+  int tingkatHafalan = 0;
   string vocab;
   string arti;
 };
@@ -31,15 +31,15 @@ void buffer() {
   }
 }
 void clearScreen() {
-  // for (int i = 0; i < 50; i++) {
-  //   cout << "\n";
-  // }
-  #ifdef WINDOWS
-    std::system("cls");
-  #else
-    // Assume POSIX
-    std::system ("clear");
-  #endif
+// for (int i = 0; i < 50; i++) {
+//   cout << "\n";
+// }
+#ifdef WINDOWS
+  std::system("cls");
+#else
+  // Assume POSIX
+  std::system("clear");
+#endif
 }
 
 void menuADD() {
@@ -49,30 +49,120 @@ void menuADD() {
 
   cout << "Enter the input : ";
 }
-void read(){
+void read() {
   IFLASHCARD.open("kartu1.txt");
   while (getline(IFLASHCARD, baris)) {
-    indeks++;
     kosakata[indeks].urutan = indeks;
     kosakata[indeks].vocab = baris.substr(0, baris.find("$"));
-    kosakata[indeks].arti = baris.substr(baris.find("$") + 1, baris.find("#")-baris.find("$")-1);
-    kosakata[indeks].tingkatHafalan = stoi(baris.substr(baris.find("#") + 1, baris.find("%")-baris.find("#")-1));
-    
+    kosakata[indeks].arti = baris.substr(baris.find("$") + 1,
+                                         baris.find("#") - baris.find("$") - 1);
+    kosakata[indeks].tingkatHafalan = stoi(baris.substr(
+        baris.find("#") + 1, baris.find("%") - baris.find("#") - 1));
+    indeks++;
   }
   IFLASHCARD.close();
 
   // for (int i = 1; i <= indeks; i++) {
-  //   cout << kosakata[i].urutan << ". " << kosakata[i].vocab << " : " << kosakata[i].arti << kosakata[i].tingkatHafalan << endl;
+  //   cout << kosakata[i].urutan << ". " << kosakata[i].vocab << " : " <<
+  //   kosakata[i].arti << kosakata[i].tingkatHafalan << endl;
   // }
 }
-void search() {
-  
-}
+void search() {}
 void review() {}
-void test() {
-  
+
+string uppercase(string s) {
+  for (int i = 0; i < s.length(); i++) {
+    s[i] = toupper(s[i]);
+  }
+  return s;
 }
-void add(){
+int handleCorrect() {
+  cout << "Correct !\n";
+  cout << "1. Next\n";
+  cout << "2. Back\n";
+  buffer();
+  mainMenuBottom();
+  int pilihan;
+  cin >> pilihan;
+  return pilihan;
+}
+int handleWrong() {
+  cout << "Wrong !\n";
+  cout << "Guess again ?\n";
+  cout << "1. Yes\n";
+  cout << "2. No\n";
+  buffer();
+  mainMenuBottom();
+  int pilihan;
+  cin >> pilihan;
+  return pilihan;
+}
+
+void pilihTest(int ctx) {
+  if (ctx == 1) {
+    int random = rand() % indeks + 1;
+    clearScreen();
+    cout << "========\tEN - ID  \t========\n";
+    cout << "========\t" << uppercase(kosakata[random].vocab) << "\t\t========\n";
+    cout << "=======================================\n";
+    buffer();
+    cout << "Your answer : ";
+    string input;
+    cin >> input;
+    if (uppercase(input) == uppercase(kosakata[random].arti)) {
+      if (handleCorrect() == 1) {
+        pilihTest(ctx);
+      }
+    } else {
+      if (handleWrong() == 1) {
+        pilihTest(ctx);
+      }
+    }
+  } else if (ctx == 2) {
+    int random = rand() % indeks + 1;
+    clearScreen();
+    cout << "========\tID - EN  \t========\n";
+    cout << "========\t" << uppercase(kosakata[random].arti) << "\t\t========\n";
+    cout << "=======================================\n";
+    buffer();
+    cout << "Your answer : ";
+    string input;
+    cin >> input;
+    if (uppercase(input) == uppercase(kosakata[random].vocab)) {
+      if (handleCorrect() == 1) {
+        pilihTest(ctx);
+      }
+    } else {
+      if (handleWrong() == 1) {
+        pilihTest(ctx);
+      }
+    }
+  }
+}
+
+void test() {
+  srand(time(0));
+  read();
+  clearScreen();
+  cout << "Test your vocabulary\n";
+  cout << "1. EN - ID\n";
+  cout << "2. ID - EN\n";
+  cout << "3. Back\n";
+  buffer();
+  mainMenuBottom();
+  int pilihan;
+  cin >> pilihan;
+  if (pilihan == 1) {
+    pilihTest(1);
+  }
+  if (pilihan == 2) {
+    pilihTest(2);
+  } else {
+    cout << "Wrong input\n";
+  }
+}
+
+void add() {
   int panjang;
   menuADD();
   OFLASHCARD.open("kartu1.txt", ios::app);
@@ -94,7 +184,7 @@ void add(){
 
 int main() {
   bool running = true;
-  while(running){
+  while (running) {
     clearScreen();
     mainMenuTop();
     buffer();
@@ -124,5 +214,3 @@ int main() {
     }
   }
 }
-
-
